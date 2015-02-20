dbb2867223f0ea40fbc1678172cb9d1ac6927fbf
Stephen Elkington <stephen.elkington@adi.tv>
2015-02-20 15:51:20 +0000

    Media Player Test v0.1

---
+++ Media Player/Media Player/Media Player/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.connectBTN = New System.Windows.Forms.Button()
+        Me.PlayVidBTN = New System.Windows.Forms.Button()
+        Me.stopVideoBTN = New System.Windows.Forms.Button()
+        Me.infoTXT = New System.Windows.Forms.TextBox()
+        Me.getVideoInfoBTN = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'connectBTN
+        '
+        Me.connectBTN.Location = New System.Drawing.Point(12, 12)
+        Me.connectBTN.Name = "connectBTN"
+        Me.connectBTN.Size = New System.Drawing.Size(75, 23)
+        Me.connectBTN.TabIndex = 0
+        Me.connectBTN.Text = "Connect"
+        Me.connectBTN.UseVisualStyleBackColor = True
+        '
+        'PlayVidBTN
+        '
+        Me.PlayVidBTN.Location = New System.Drawing.Point(103, 12)
+        Me.PlayVidBTN.Name = "PlayVidBTN"
+        Me.PlayVidBTN.Size = New System.Drawing.Size(75, 23)
+        Me.PlayVidBTN.TabIndex = 1
+        Me.PlayVidBTN.Text = "Play Video"
+        Me.PlayVidBTN.UseVisualStyleBackColor = True
+        '
+        'stopVideoBTN
+        '
+        Me.stopVideoBTN.Location = New System.Drawing.Point(103, 41)
+        Me.stopVideoBTN.Name = "stopVideoBTN"
+        Me.stopVideoBTN.Size = New System.Drawing.Size(75, 23)
+        Me.stopVideoBTN.TabIndex = 2
+        Me.stopVideoBTN.Text = "Stop Video"
+        Me.stopVideoBTN.UseVisualStyleBackColor = True
+        '
+        'infoTXT
+        '
+        Me.infoTXT.Location = New System.Drawing.Point(3, 181)
+        Me.infoTXT.Multiline = True
+        Me.infoTXT.Name = "infoTXT"
+        Me.infoTXT.Size = New System.Drawing.Size(277, 320)
+        Me.infoTXT.TabIndex = 3
+        '
+        'getVideoInfoBTN
+        '
+        Me.getVideoInfoBTN.Location = New System.Drawing.Point(184, 12)
+        Me.getVideoInfoBTN.Name = "getVideoInfoBTN"
+        Me.getVideoInfoBTN.Size = New System.Drawing.Size(75, 23)
+        Me.getVideoInfoBTN.TabIndex = 4
+        Me.getVideoInfoBTN.Text = "Get info"
+        Me.getVideoInfoBTN.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(172, 70)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 513)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.getVideoInfoBTN)
+        Me.Controls.Add(Me.infoTXT)
+        Me.Controls.Add(Me.stopVideoBTN)
+        Me.Controls.Add(Me.PlayVidBTN)
+        Me.Controls.Add(Me.connectBTN)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents connectBTN As System.Windows.Forms.Button
+    Friend WithEvents PlayVidBTN As System.Windows.Forms.Button
+    Friend WithEvents stopVideoBTN As System.Windows.Forms.Button
+    Friend WithEvents infoTXT As System.Windows.Forms.TextBox
+    Friend WithEvents getVideoInfoBTN As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Media Player/Media Player/Media Player/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports CasparCGNETConnector
+'get info from https://github.com/mcdikki/CasparCGNetConnector/wiki/Overview
+Public Class Form1
+
+    Dim WithEvents con As New CasparCGConnection("localhost", 5250)
+
+    Private Sub connectBTN_Click(sender As Object, e As EventArgs) Handles connectBTN.Click
+        connectBTN.BackColor = Color.Red
+        ' addHandler(con.disconnected, AddressOf onDisconnect)
+        'addHandler(con.connected, AddressOf onConnect)
+        con.connect()
+        If Me.con.isConnected = False Then
+            Me.con.connect()
+            If Me.con.isConnected = False Then
+                connectBTN.BackColor = Color.Green
+            End If
+        End If
+        If Me.con.isConnected = True Then
+
+            connectBTN.BackColor = Color.Green
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles PlayVidBTN.Click
+        Dim cmd As New PlayCommand(1, 1, "amb", True)
+        If cmd.execute(con).isOK Then
+            infoTXT.Text = "Yeah, it's playing!"
+        Else
+            infoTXT.Text = "Oh no, something went wrong. Let's check the the server message: " + cmd.getResponse.getServerMessage()
+        End If
+    End Sub
+
+    Private Sub stopVideoBTN_Click(sender As Object, e As EventArgs) Handles stopVideoBTN.Click
+        Dim cmd As New StopCommand(1, 1)
+        cmd.execute(con)
+    End Sub
+
+    Private Sub getVideoInfoBTN_Click(sender As Object, e As EventArgs) Handles getVideoInfoBTN.Click
+        Dim media As New CasparCGMovie("amb")
+        media.fillMediaInfo(con)
+        infoTXT.Text = "All media infos we got from the server:"
+        For Each info As String In media.Infos.Keys
+            infoTXT.Text = infoTXT.Text + vbCrLf + vbTab & info & ": " & media.getInfo(info)
+        Next
+        Dim cmd As New PlayCommand(1, 1, media)
+        cmd.execute(con)
+    End Sub
+
+    Private Sub showBase64Image(p1 As Object)
+        Throw New NotImplementedException
+    End Sub
+
+End Class
